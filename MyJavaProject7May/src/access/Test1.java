e2fd5ae0e4c9efcd429e5f3744e60fa1a65d3ea9
shri <shri@DESKTOP-DJ0KA9U>
2022-08-26 18:18:54 +0530

    Changes

--- MyJavaProject7May/src/access/Test1.java
+++ MyJavaProject7May/src/access/Test1.java
@@ -4,5 +4,8 @@ public class Test1 {
 public static void main(String[] args) {
 	Encaps obj=new Encaps();	
 	System.out.println(obj.balance);
+	System.out.println("Vidya Change");
+	System.out.println("Vidya Change");
+	
 }
 }
